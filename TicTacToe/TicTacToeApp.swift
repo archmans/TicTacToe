5367aa8dda5f7db96b97f9bacffb04c4a41ea867
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2023-05-31 22:52:22 +0700

    Add : Project, Config

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Muhamad Salman Hakim Alfarisi on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
